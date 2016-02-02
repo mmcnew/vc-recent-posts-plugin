eaf6442532d2a9f1ba46aaea303cbffdde1f6ce5
mmcnew <mike@visceralconcepts.com>
2016-02-01 22:22:28 -0800

    Massive Overhaul
    Made Major Changes to allow for variables.

--- vc-recent-posts.php
+++ vc-recent-posts.php
@@ -3,7 +3,7 @@
   Plugin Name: VC Recent Posts Shortcode
   Plugin URI: http://www.visceralconcepts.com
   Description: A shortcode to add a stylized list of recent posts to any place on your site.
-  Version: 1.01
+  Version: 1.02
   Author: Visceral Concepts
   Author URI: http://www.visceralconcepts.com
   License: GPLv3 or Later
@@ -26,7 +26,7 @@ if ( is_admin() ) {
 
 //Load CSS & Scripts
 
-add_action( 'wp_enqueue_scripts', 'rp_scripts', 17 );
+add_action( 'wp_enqueue_scripts', 'rp_scripts' );
 function rp_scripts() {
 
 	wp_register_style( 'rp-css', plugin_dir_url(__FILE__) . 'css/recent-post-styles.css' );
@@ -37,24 +37,90 @@ function rp_scripts() {
 // Shortcode Construct
 
 function vc_recent_posts_shortcode($atts){
- $q = new WP_Query(
-   array( 'orderby' => 'date', 'posts_per_page' => '6')
- );
-
-$list = '<div class="recent-posts">';
-
-while($q->have_posts()) : $q->the_post();
-	$count++;
-	$feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-	$list .= '<div><a href="' . get_permalink() . '"><img src="' . $feat_image . '" alt="' . get_the_title() . '"/><p class="date">' . get_the_date() . '</p></a></div>';
-	if ( 0 == $count%3 ) {
-        $list .= '</div><div class="recent-posts">';
-    }
-endwhile;
-
-wp_reset_query();
-
-return $list . '</div> <a href="' . get_permalink( get_option( 'page_for_posts' ) ) . '" class="button more">View More</a>';
+	
+	$a = shortcode_atts ( array (
+		'type' =>  'posts',
+		'style' => 'grid',
+		'columns' => '3',
+		'image' => 'true',
+		'title' => 'true',
+		'excerpt' => 'false',
+		'date' => 'false',
+		'link' => 'true',
+		'feat-bg' => 'true',
+		'classes' => ''
+		), $atts );
+		
+	$args = array(
+		'orderby' => 'date',
+		'posts_per_page' => '6'
+	);
+	
+	$q = new WP_Query( $args);
+	
+	$list = '<div id="recent-posts"><div class="recent-posts ' . $a['style'] . '">';
+	while($q->have_posts()) : $q->the_post();
+		if ( $a['style'] != 'list' ) {
+			$count++;
+		}
+		$feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+		$excerpt =  get_the_excerpt();
+		$link = get_permalink();
+		$title = get_the_title();
+		$date = get_the_date();
+		
+		$list .= '<div class="post';
+		if ( $a['image'] == 'false' && $a['feat-bg'] == 'false' ) {
+			$list .= ' bborder';
+		}
+		$list .= '"';
+		if ( $a['feat-bg'] != 'false' ) {
+			$list .= ' style="background-image: url(' . $feat_image . ')"';
+		}
+		$list .= '>';
+		if ( $a['link'] != 'false' ) {
+			$list .= '<a href="' . $link . '">';
+		}
+		if ( $a['title'] != 'false' ) {
+			$list .= '<h3>' . $title . '</h3>';
+		}
+		if ( $a['link'] != 'false' ) {
+			$list .= '</a>';
+		}
+		if ( $a['link'] != 'false' ) {
+			$list .= '<a href="' . $link . '">';
+		}
+		if ( $a['image'] != 'false' && $a['feat-bg'] != 'true' ) {
+			$list .= '<img src="' . $feat_image . '" alt="' . $title . '"/>';
+		}
+		if ( $a['link'] != 'false' ) {
+			$list .= '</a>';
+		}
+		if ( $a['link'] != 'false' ) {
+			$list .= '<a href="' . $link . '">';
+		}
+		if ( $a['date'] != 'false' ) {
+			$list .= '<p class="date">' . $date . '</p>';
+		}
+		if ( $a['link'] != 'false' ) {
+			$list .= '</a>';
+		}
+		if ( $a['excerpt'] != 'false' ) {
+			$list .= '<div class="excerpt"><p>' . $excerpt . '</p></div>';
+		}
+		$list .= '</div>';
+		if ( $a['style'] != 'list' ) {
+			if ( $a['columns'] == 3 && 0 == $count%3 ) {
+				$list .= '</div><div class="recent-posts ' . $a['style'] . '">';
+			} elseif ( $a['columns'] == 4 && 0 == $count%4 ) {
+				$list .= '</div><div class="recent-posts ' . $a['style'] . '">';
+			}
+		}
+	endwhile;
+	
+	wp_reset_query();
+	
+	return $list . '</div> <a href="' . get_permalink( get_option( 'page_for_posts' ) ) . '" class="button more">View More</a> </div>';
 
 }
 
